df5872fa31be50f7ecf2e36f3d9d537cfd8129f9
timlindberg77 <tim.lindberg888@gmail.com>
2026-09-16 09:41:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace uppgift2.inkapsling.sep16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ klasser/BankAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uppgift2.inkapsling.sep16.klasser
+{
+    internal class BankAccount
+    {
+    }
+}
